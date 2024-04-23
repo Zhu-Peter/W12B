638629413d018a17956ff10de15e856ffbeb361f
Zhu-Peter <gzhu@ualberta.ca>
2024-04-23 15:16:42 -0600

    added price filter

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 document.getElementById(`button_request`).addEventListener(`click`, getActivity);
 
-webURL = `http://www.boredapi.com/api/activity/`;
-webFilter = ``;
+let webURL = `http://www.boredapi.com/api/activity/`;
+let webFilter = ``;
 
 function getActivity(){
+    webFilter = ``;
+    let filter_price = document.getElementById(`filter_price`).value;
+    if (filter_price == `none`){
+        webFilter = ``;
+    }else if(filter_price == `free`){
+        webFilter = `?price=0.0`;
+    }else{
+        webFilter = `?price=`+ filter_price;
+    }
     axios.request({  
         url: webURL + webFilter,  
     }).then(successFunction).catch(failureFunction);
 }
 
 function successFunction(request){
-    console.log(request)
+    // console.log(request)
+
     document.getElementById(`activity_container`).innerHTML = ``;
     document.getElementById(`activity_container`).insertAdjacentHTML(`beforeend`, `
     <h1>${request.data.activity}</h1>
-    <h2>$${request.data.price}</h2>
+    <h2>${request.data.price ? `$`+ request.data.price : `Free`}</h2>
     <h3><a href="${request.data.link}">${request.data.link}</a></h3>
     `)
 }
